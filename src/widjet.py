90d5d8b8fda856b7be95f8640e8820047f530b88
Kosta Valeev <konstantin.s.valeev@gmail.com>
2025-07-13 21:27:31 -0500

    final 7/13

--- src/widjet.py
+++ src/widjet.py
@@ -1,5 +1,6 @@
-from masks import get_mask_card_number, get_mask_account
 from datetime import datetime
+
+from masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(info: str) -> str:
